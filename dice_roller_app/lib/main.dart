106d2ee723c2eb0f73df19646585595ab2a0a030
Vanshita <120383319+vanshita-80048@users.noreply.github.com>
2023-10-27 18:28:23 +0530

    add child widgets

--- dice_roller_app/lib/main.dart
+++ dice_roller_app/lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 190, 166, 233),
         body: Center(
           child: Text('Hello, How r u?'),
         ),
