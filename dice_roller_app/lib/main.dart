1d7ef3fa74cf82faade25e6badbfa91693d0881b
Vanshita <120383319+vanshita-80048@users.noreply.github.com>
2023-10-27 20:40:30 +0530

    add constructor func for custom widget

--- dice_roller_app/lib/main.dart
+++ dice_roller_app/lib/main.dart
@@ -2,34 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: 
+        body: GradientContainer(),
       ),
     ),
   ); //provided by flutter
 }
 
-class GradientContainer extends StatelessWidget{
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
-     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 180, 110, 89),
-                Color.fromARGB(255, 27, 170, 27)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello, How r u?',
-              style: TextStyle(fontSize: 28, color: Colors.white),
-            ),
-          ),
-        );
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 180, 110, 89),
+            Color.fromARGB(255, 27, 170, 27)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello, How r u?',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+      ),
+    );
   }
 }
