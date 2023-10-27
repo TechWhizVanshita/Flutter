147d721318752d6875b15bffe9f52c094b08ef4d
Vanshita <120383319+vanshita-80048@users.noreply.github.com>
2023-10-27 21:38:13 +0530

    add variables

--- dice_roller_app/lib/gradient_container.dart
+++ dice_roller_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'style_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 180, 110, 89),
             Color.fromARGB(255, 27, 170, 27)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
