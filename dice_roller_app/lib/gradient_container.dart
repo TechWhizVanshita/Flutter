263931b5b80e34494d70e56c468f07e283337104
vanshita80048 <vanshita479@gmail.com>
2023-11-01 21:53:38 +0530

    commit

--- dice_roller_app/lib/gradient_container.dart
+++ dice_roller_app/lib/gradient_container.dart
@@ -1,15 +1,12 @@
 import 'package:dice_roller_app/dice_roller.dart';
 import 'package:flutter/material.dart';
+import 'style_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
-
-  const GradientContainer.purple({super.key})
-      : color1 = Colors.deepPurple,
-        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -21,7 +18,7 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
           colors: [color1, color2],
           begin: startAlignment,
-        
+          end: endAlignment,
         ),
       ),
       child: const Center(
